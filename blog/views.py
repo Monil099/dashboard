8c9ac04b41592cf581a47643a7e333b247961bae
Monil <monilsojitra903@gmail.com>
2024-09-25 02:10:50 +0530

    update blog & maintain other module

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 from .models import WpBlog, WpSite, WpBlogSite
 import base64
 import requests
+from blog.categories import categories_list
 
 def home_view(request):
     return render(request, 'blog_list.html')
@@ -27,8 +28,12 @@ class CreateBlogPostView(LoginRequiredMixin, CreateView):
         form.instance.created_by = user
         form.instance.updated_by = user
         self.object = form.save()  # Save the blog post to the database
-
         sites = self.request.POST.getlist('sites')
+        categories = self.request.POST.get("categories")
+        category_id = None
+        for category, id in categories_list.items():
+            if category == categories:
+                category_id = id
         result = {}
         for site_id in sites:
             site = WpSite.objects.filter(id=int(site_id)).first()
@@ -47,6 +52,7 @@ class CreateBlogPostView(LoginRequiredMixin, CreateView):
                 'status': form.cleaned_data['status'],
                 'slug': 'example-post',
                 'content': form.cleaned_data['content'],
+                "categories": category_id
             }
 
             response = requests.post(api_url, headers=wordpress_header, json=data)
@@ -76,37 +82,40 @@ class BlogListView(ListView):
         return WpBlog.objects.prefetch_related('wpblogsite_set__site').all()
 
 
-class BlogByIdView(View):
-    
+class BlogByIdView(LoginRequiredMixin, View):
     def get(self, request, blog_id, *args, **kwargs):
         # Fetch the blog post by its ID
         blog = get_object_or_404(WpBlog, id=blog_id)
+        
+        sites = WpSite.objects.all().values("id", "name")
+        selected_sites = list(WpBlogSite.objects.filter(blog_id=blog_id).values_list("site__id", flat=True))
+
         # Return the blog details as a context for a template
-        return render(request, 'blog_detail.html', {'blog': blog})
+        return render(request, 'blog_detail.html', {'blog': blog, 'sites': sites, 'selected_sites': selected_sites})
 
-class UpdateBlogPostView(LoginRequiredMixin, UpdateView):
-    model = WpBlog
-    template_name = 'blog_detail.html'
-    fields = ['title', 'content', 'status']  # Define fields for the form
-
-    def get_context_data(self, **kwargs):
-        # Add additional context (like available sites) to the template
-        context = super().get_context_data(**kwargs)
-        context['sites'] = WpSite.objects.all().values("id", "name")
-        return context
-
-    def form_valid(self, form):
+class BlogUpdateView(views.View):
+    def post(self, request, pk, *args, **kwargs):
         user = self.request.user
-        form.instance.created_by = user
-        form.instance.updated_by = user
-        self.object = form.save()  # Save the blog post to the database
+        blog = get_object_or_404(WpBlog, id=int(pk))
+        title = request.POST.get('title', blog.title)
+        content = request.POST.get('content', blog.content)
+        status = request.POST.get('status', blog.status)
 
-        sites = self.request.POST.getlist('sites')
-        result = {}
+        blog.title = title
+        blog.content = content
+        blog.status = status
+        blog.updated_by = user
+        blog.save()
+
+        sites = self.request.POST.getlist('sites')  # Get the selected sites from the form
+        result = {}  # To store the result of each site's response
+
         for site_id in sites:
-            site = WpSite.objects.filter(id=int(site_id)).first()
+            site = get_object_or_404(WpSite, id=int(site_id))  # Fetch the site
+            data = WpBlogSite.objects.filter(blog=blog, site=site_id)
+            # (WpBlog, site_id=int(site_id))  # Fetch the site
 
-            # Make the request to the WordPress site
+            # Prepare WordPress API credentials
             wordpress_url = site.url
             wordpress_user = site.wp_username
             wordpress_password = site.wp_password
@@ -114,29 +123,41 @@ class UpdateBlogPostView(LoginRequiredMixin, UpdateView):
             wordpress_token = base64.b64encode(wordpress_credentials.encode())
             wordpress_header = {'Authorization': 'Basic ' + wordpress_token.decode('utf-8')}
 
-            api_url = f'{wordpress_url}/wp-json/wp/v2/posts'
-            data = {
-                'title': form.cleaned_data['title'],
-                'status': form.cleaned_data['status'],
-                'slug': 'example-post',
-                'content': form.cleaned_data['content'],
-            }
+            blog_sites = WpBlogSite.objects.filter(blog=blog).all()
 
-            response = requests.post(api_url, headers=wordpress_header, json=data)
+            for blog_site in blog_sites:
+                categories = self.request.POST.get("categories")
+                category_id = None
+                for category, id in categories_list.items():
+                    if category == categories:
+                        category_id = id
+                wordpress_post_id = blog_site.wp_blog_id
+                api_url = f'{wordpress_url}/wp-json/wp/v2/posts/{wordpress_post_id}'
+                data = {
+                    'title': title,
+                    'status': status,
+                    'content': content,
+                    "categories": category_id
+                }
 
-            # Handle the response and update the result context
-            if response.status_code != 201:
-                result = {"msg-error": f"Failed to upload blog to {wordpress_url}"}
+                # Make the request to the WordPress site
+                response = requests.patch(api_url, headers=wordpress_header, json=data)
 
-            else:
-                blog_json = response.json()
-                wp_blog_id = blog_json["id"]
-                wp_blog_url = blog_json['guid']['rendered']
-                result = {"msg": f"Successfully uploaded blog to {wordpress_url}", "url": wp_blog_url}
-                # Save the relationship between the blog and the sites
-                WpBlogSite.objects.get_or_create(blog=self.object, site_id=int(site_id), wp_blog_id=wp_blog_id)
+                # Handle the response and update the result context
+                if response.status_code != 201:
+                    result[site_id] = {"msg-error": f"Failed to upload blog to {wordpress_url}"}
+                else:
+                    blog_json = response.json()
+                    wp_blog_id = blog_json["id"]
+                    wp_blog_url = blog_json['guid']['rendered']
+                    result[site_id] = {"msg": f"Successfully uploaded blog to {wordpress_url}", "url": wp_blog_url}
+                    # Save the relationship between the blog and the sites
+                    WpBlogSite.objects.get_or_create(blog=blog, site=site, wp_blog_id=wp_blog_id)
 
-        return self.render_to_response(self.get_context_data(form=form, result=result))
+        # Pass the result to the context and redirect
+        self.request.session['result'] = result  # Store the result in session for display
+        return redirect('blog_list')  # Redirect to the detail page or another view
+
 
 class BlogDeleteView(views.View):
     def get(self, request, pk, *args, **kwargs):
