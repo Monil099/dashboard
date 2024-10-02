8bd8d1ccd23496e1c3b4f8a986d1ef55a6e5f610
meet <meetbhingradiya57@gmail.com>
2024-10-02 17:02:14 +0530

    update setting.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+7=z^pimqq&4vg5&m%zvo3f5m#yukrf^$!t)s+0m2#sifo%2ie
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'helpcenter.airhubapp.com.com']
 
 
 # Application definition
