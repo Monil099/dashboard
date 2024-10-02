d26c3792ab5cb08c5df85eaf399f6045d2694924
meet <meetbhingradiya57@gmail.com>
2024-10-02 18:48:37 +0530

    update user path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('accounts/', include('user.urls')),
     path('', include('blog.urls')),
 ]
--- wp_site/models.py
+++ wp_site/models.py
@@ -7,4 +7,6 @@ class WpSite(models.Model):
     wp_username = models.CharField(max_length=200)
     wp_password = models.CharField(max_length=200)
 
-# WpSite.objects.create(name="Airhubapp JP", url="https://support.airhubapp.jp/", wp_username="meetbhingradiya57@gmail.com", wp_password="NC3Q Oqwu tc8o XN82 4Yz6 FATW")
+# WpSite.objects.create(name="support.airhubapp.jp", url="https://support.airhubapp.jp", wp_username="meetbhingradiya57@gmail.com", wp_password="NC3Q Oqwu tc8o XN82 4Yz6 FATW")
+# WpSite.objects.create(name="support.airhubapp.com", url="https://support.airhubapp.com", wp_username="meetbhingradiya57@gmail.com", wp_password="NC3Q Oqwu tc8o XN82 4Yz6 FATW")
+# WpSite.objects.create(name="support.esimcards.co.uk", url="https://support.esimcards.co.uk", wp_username="meetbhingradiya57@gmail.com", wp_password="NC3Q Oqwu tc8o XN82 4Yz6 FATW")
